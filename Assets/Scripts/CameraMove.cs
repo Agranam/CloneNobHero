47c02086cd176d24ca6c028c5590c785f7e240ad
Agranam <mragranam@mail.ru>
2023-05-04 04:43:20 +0300

    Added move for Player and some asstes

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+
+    private void LateUpdate()
+    {
+        transform.position = _target.position;
+    }
+}
---
+++ Assets/Scripts/RidgidbodyMove.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class RidgidbodyMove : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Joystick _joystick;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _speed = 5f;
+
+    private Vector2 _moveInput;
+    private readonly int _run = Animator.StringToHash("Run");
+
+    private void Update()
+    {
+        _moveInput = _joystick.Value;
+
+        if (_moveInput == Vector2.zero)
+        {
+            _animator.SetBool(_run, false);
+        }
+        else
+        {
+            _animator.SetBool(_run, true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector3(_moveInput.x, 0f, _moveInput.y) * _speed;
+
+        if (_rigidbody.velocity != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(_rigidbody.velocity, Vector3.up);
+        }
+    }
+}
