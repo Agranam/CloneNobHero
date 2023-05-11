34bc589d34f8e38adce21249c60f1f79b4b330a2
Agranam <mragranam@mail.ru>
2023-05-12 00:53:37 +0300

    Added Enemy

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private Transform _playerTransform;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rotationLerpRate = 3f;
+
+
+    private void FixedUpdate()
+    {
+        if (_playerTransform)
+        {
+            Vector3 toPlayer = _playerTransform.position - transform.position;
+
+            Quaternion toPlayerRotation = Quaternion.LookRotation(toPlayer, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, toPlayerRotation, Time.deltaTime * _rotationLerpRate);
+
+            _rigidbody.velocity = transform.forward * _speed;
+        }
+    }
+}
